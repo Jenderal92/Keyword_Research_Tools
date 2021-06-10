2fe0837c50580a1320af38c9725c11282bb979a0
Jenderal92 <59664965+Jenderal92@users.noreply.github.com>
2021-06-10 19:44:52 +0700

    Add files via upload

---
+++ sugest.py
@@ -0,0 +1,15 @@
+#Keyword Research Tool
+import requests
+from time import time as timer  
+import time
+from re import findall as reg
+year = time.strftime("%d/%m/%y")
+
+
+text = raw_input('\033[31mKeyword Research Tool: ')
+
+test = requests.get('http://suggestqueries.google.com/complete/search?output=toolbar&hl=id&q='+text).content
+if '<toplevel>' in test:
+	regx = reg('<suggestion data="(.*?)"/>', test)
+	for regs in regx:
+		print('\033[0mResult :' +' ' + '\033[32m'+regs)
